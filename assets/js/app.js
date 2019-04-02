7c9acad44059775ad12888cd23d793b1482380e9
The_Costa <bryan.costa@roadrunner.com>
2019-04-01 19:32:58 -0700

    Added icon

--- assets/js/app.js
+++ assets/js/app.js
@@ -7,3 +7,15 @@ const uniData = [{
     id: 110653 //replace id
 }
 ]
+
+fetch('https://api.openweathermap.org/data/2.5/weather?q=Bujumbura,Burundi&units=imperial&appid=5c221ff448d1c7f08a04de1abb0653aa')
+    .then(r => r.json())
+    .then(r => {
+        console.log(r);
+    })
+
+fetch(`https://api.openweathermap.org/data/2.5/forecast?lat=37.8719&lon=-122.2585&units=imperial&appid=5c221ff448d1c7f08a04de1abb0653aa`)
+    .then(r => r.json())
+    .then(r => {
+        console.log(r);
+    })
