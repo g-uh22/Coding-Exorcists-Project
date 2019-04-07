47ee15a000f90c6121c408f6fb17d655b6f002af
The_Costa <bryan.costa@roadrunner.com>
2019-04-07 14:38:51 -0700

    Added Card Code from Bootstrap_Cards.HTML

--- assets/js/app.js
+++ assets/js/app.js
@@ -183,3 +183,75 @@ schoolData()
 
 
   autocomplete(document.getElementById("myInput"), schools, schoolsId);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Carousel JS
+$(document).ready(function () {
+  $("#myCarousel").on("slide.bs.carousel", function (e) {
+    var $e = $(e.relatedTarget);
+    var idx = $e.index();
+    var itemsPerSlide = 3;
+    var totalItems = $(".carousel-item").length;
+
+    if (idx >= totalItems - (itemsPerSlide - 1)) {
+      var it = itemsPerSlide - (totalItems - idx);
+      for (var i = 0; i < it; i++) {
+        // append slides to end
+        if (e.direction == "left") {
+          $(".carousel-item")
+            .eq(i)
+            .appendTo(".carousel-inner");
+        } else {
+          $(".carousel-item")
+            .eq(0)
+            .appendTo($(this).find(".carousel-inner"));
+        }
+      }
+    }
+  });
+});
+// Forces User to click arrow to change card
+$('.carousel').carousel({
+  interval: false
+})
